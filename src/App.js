e06adf8f9404b811377d3c6c54edd8ba67384d02
alyoneek <alyonta03@mail.ru>
2023-01-14 20:50:10 +0700

    Add RegisterPage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column vh-100">
       <Navbar />
-
+      <div className="container flex-grow-1">
+        <RegisterPage />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import RegisterCard from '../components/RegisterCard';
+
+const RegisterPage = () => {
+  return (
+    <div className="d-flex h-100 justify-content-center align-items-center">
+      <RegisterCard />
+    </div>
+  );
+};
+
+export default RegisterPage;
